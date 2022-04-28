449b737a90cfe812a1e39e09b1b72273cbab751d
王鑫凯 <wangxinkai@haier.com>
2022-04-28 11:19:30 +0800

    chore: update bundle settings

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+// this babel setting is for jest, not rollup
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
+};
